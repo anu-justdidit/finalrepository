87d7a126018d9aa302dea2dea4a0bbdfd6d4fd4e
anu-justdidit <142309770+anu-justdidit@users.noreply.github.com>
2023-11-24 20:17:46 +0530

    Add files via upload

---
+++ Flask_Assignment1_Folder/folder_q1/test1.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Define a route for the homepage
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Run the app
+    app.run(debug=True)
